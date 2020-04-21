4d79c4a282f77f3983eed9b9e87be47a7883785a
Sean Daryanani <sean.daryanani@nb-sdaryanani.local>
2020-04-21 09:16:28 +0200

    Add docker workflow to project

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Section 1 - install dependencies and run npm run build command
+FROM node:alpine as builder
+WORKDIR /app
+COPY ${PWD}/package.json .
+RUN yarn
+COPY . .
+RUN yarn build
+
+# Section 2 - Install nginx and copy over output of build
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to r elo ad.
         </p>
         <a
           className="App-link"
